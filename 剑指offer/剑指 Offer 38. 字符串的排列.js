b83e9272492572832faa77e6c09162a002975941
EvilMoOd <962475158@qq.com>
2022-08-28 13:50:02 +0800

    8.27

--- 剑指offer/剑指 Offer 38. 字符串的排列.js
+++ 剑指offer/剑指 Offer 38. 字符串的排列.js
@@ -3,22 +3,20 @@
  * @return {string[]}
  */
 var permutation = function (s) {
-  // TODO 改日在做
+  // 数学中的排列问题，用程序实现就是递归
+  // 固定一个字母排列剩下字母，由小字符串逐渐排成大字符串
   if (s.length <= 1) return [s];
-  // 递归出口
   const res = [];
   for (let i = 0; i < s.length; i++) {
-    // 取出一个字符为char
-    const char = s[i];
-    // newStr=去掉char后剩下的字符
-    let newStr = s.slice(0, i) + s.slice(i + 1);
-    // 递归产生newStr的全排列
-    const next = permutation(newStr);
-    // 将char与newStr的全排列拼接，放入res
-    next.forEach((item) => {
-      res.push(char + item);
+    // 取出一个字符为char，然后把剩下的字符往下递归排列组合
+    const pickOne = s[i];
+    // 往下递归，返回每一次排列好的字符串数组
+    const other = permutation(s.slice(0, i) + s.slice(i + 1));
+    // 然后将每种组合与首字符串拼接，添加成新的字符串组
+    other.forEach((item) => {
+      res.push(pickOne + item);
     });
   }
-  // 去重
+  // 考虑到字符串中字符可能重复，所以去重处理
   return [...new Set(res)];
 };
