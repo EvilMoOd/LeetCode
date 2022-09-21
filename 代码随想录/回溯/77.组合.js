aaf21a16398bd6102e7926198aede9dd4f297f06
EvilMoOd <962475158@qq.com>
2022-09-21 15:57:28 +0800

    9.21

--- 代码随想录/回溯/77.组合.js
+++ 代码随想录/回溯/77.组合.js
@@ -11,22 +11,23 @@
  * @return {number[][]}
  */
 var combine = function (n, k) {
-  //回溯=循环+递归
-  let resArr = [];
-  function backTracking(startIndex, tempArr) {
+  //回溯=循环+递归,本质上也是枚举，不过是树的枚举
+  // 本体主要是递归枚举长度为k的数组组合
+  let res = [];
+  function backTracking(startIndex, arr) {
     // 数组长度等于k时为终止条件，加入res数组中
-    if (tempArr.length === k) {
-      resArr.push(tempArr.slice());
+    if (arr.length === k) {
+      res.push(arr.slice());
       return;
     }
-    for (let i = startIndex; i <= n - k + 1 + tempArr.length; i++) {
-      tempArr.push(i);
-      backTracking(i + 1, tempArr);
-      tempArr.pop();
+    for (let i = startIndex; i <= n - k + 1 + arr.length; i++) {
+      arr.push(i);
+      backTracking(i + 1, arr);
+      arr.pop();
     }
   }
   backTracking(1, []);
-  return resArr;
+  return res;
   
 };
 // @lc code=end
