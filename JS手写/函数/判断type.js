2807179e1a8e2bd9bd25bb35999de3d6ceb5a2f4
EvilMoOd <962475158@qq.com>
2022-11-10 11:50:55 +0800

    转移手写至新项目中，该项目只用于力扣练习数据结构与算法
    1

--- JS手写/函数/判断type.js
+++
@@ -1,7 +0,0 @@
-function DataType(data, type) {
-  const dataType = Object.prototype.toString
-    .call(data)
-    .replace(/\[object (\w+)\]/, '$1')
-    .toLowerCase();
-  return type ? dataType === type : dataType;
-}
